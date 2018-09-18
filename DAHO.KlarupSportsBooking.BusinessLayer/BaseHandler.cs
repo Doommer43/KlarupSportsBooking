666b6fade8ff30774a8f4918c06a7b607e644e8c
Daniel Nicolas Birk Holm <dani479d@edu.campusvejle.dk>
2018-09-18 09:31:07 +0200

    Added the majority of projects to the solution and the BaseHandler, with logic, to BusinessLayer.

---
+++ DAHO.KlarupSportsBooking.BusinessLayer/BaseHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAHO.KlarupSportsBooking.DateAccessLayer.EF;
+
+namespace DAHO.KlarupSportsBooking.BusinessLayer
+{
+    public class BaseHandler
+    {
+        private KlarupHalBookingDBModel model = new KlarupHalBookingDBModel();
+
+        protected KlarupHalBookingDBModel Model { get => model; set => model = value; }
+
+        public bool SaveChanges()
+        {
+            int rowsAffected = Model.SaveChanges();
+
+            if(rowsAffected > 0)
+            {
+                return true;
+            } return false;
+        }
+    }
+}
